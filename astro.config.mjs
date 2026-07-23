46997122b333e62d4c5dea634a1eb7509fbcac33
Kagiel <cxin1627@gmail.com>
2026-07-24 06:08:17 +0900

    fix: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://Kagiel-snow.github.io",
+  site: "https://kagiel-snow.github.io",
   base: "/kagiel-blog/",
   trailingSlash: "always",
   integrations: [
